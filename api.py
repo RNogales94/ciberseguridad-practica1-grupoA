c5d7f653b0ee27b7b4c1bace1a5bec7016029cef
RNogales94 <rafael.nogales@shell.com>
2023-09-09 19:00:27 +0200

    Add post tickers

--- api.py
+++ api.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask import request
 
-from yahoo_data_fetcher import get_price
+from yahoo_data_fetcher import get_price, set_price
 
 
 app = Flask(__name__)
@@ -12,9 +12,15 @@ def hello_world():
     return "<p>Hello, World!</p>"
 
 
-@app.route("/api/<ticker>")
-def api(ticker):
+@app.route("/api/<ticker>", methods=["GET"])
+def get_ticker(ticker):
     return get_price(ticker)
+
+
+@app.route("/api/<ticker>", methods=["POST"])
+def post_ticker(ticker):
+    document = get_price(ticker)
+    return set_price(document)
 
 
 @app.route("/api/multiple/")
